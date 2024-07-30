66e31f8759afcfc09ec4018db26ba49de22117b5
000brian <brianmaciejwilk@gmail.com>
2024-07-29 19:11:02 -0500

    Added dimension button and drawing effect for grid

--- script.js
+++ script.js
@@ -2,21 +2,47 @@ let gridDimension = 4;
 
 const container = document.querySelector("div#container");
 
-
-const square = document.createElement("div");
-square.classList.toggle("square");
-
-for (let i = 0; i < gridDimension; i++)
+function createGrid(gridDimension)
 {
-    const row = document.createElement("div");
-    row.classList.toggle("row");
+    const square = document.createElement("div");
+    square.classList.toggle("square");
+    
     for (let i = 0; i < gridDimension; i++)
     {
-        const square = document.createElement("div");
-        square.classList.toggle("square");
-        row.appendChild(square);
+        const row = document.createElement("div");
+        row.classList.toggle("row");
+        for (let i = 0; i < gridDimension; i++)
+        {
+            const square = document.createElement("div");
+            square.classList.toggle("square");
+            square.addEventListener("mouseover", () =>
+            {
+                square.style.backgroundColor = "red";
+            });
+            row.appendChild(square);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
 }
+createGrid(50);
 
 
+const button = document.querySelector("button#dimension-button");
+button.addEventListener("click", () =>
+{
+    let gridDimension = +prompt("wat dimension do u want the grid to be");
+    if (gridDimension > 100)
+    {
+        alert("thats way too much man try again")
+    }
+    else if (typeof gridDimension != "number")
+    {
+        alert("thats not a number cmon try again")
+    }
+    else
+    {
+        container.replaceChildren();
+        createGrid(gridDimension);
+    }
+
+});
